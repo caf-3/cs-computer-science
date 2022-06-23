ddc5ca3b3c12ab7cbc7ff6f17edc225ab40fc3eb
caf-3 <00caf3@gmail.com>
2022-06-23 07:42:23 +0200

    folders moved to 01-building-abstractions-with-functions

--- 01-programming/composing-programs/02-elements-of-programming/02-names-and-environment.py
+++
@@ -1,5 +0,0 @@
-# We can use assignment statements to give new names to existing functions.
-from math import pi
-f = max
-radius = 10
-area, circumference = pi * radius * radius, 2 * pi * radius
--- 01-programming/composing-programs/06-recursive-functions/03-advanced.py
+++
@@ -1,4 +0,0 @@
-def reverseString(str):
-    if len(str) != 0:
-        print(str[-1])
-        reverseString(str[:-1])
