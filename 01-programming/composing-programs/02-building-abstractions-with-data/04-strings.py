1f33ceab2dd96ac31b2dcbc589c5ccaa28631e1a
caf-3 <00caf3@gmail.com>
2022-09-26 16:05:02 +0200

    data structures added

--- 01-programming/composing-programs/02-building-abstractions-with-data/04-strings.py
+++ 01-programming/composing-programs/02-building-abstractions-with-data/04-strings.py
@@ -1,3 +1,5 @@
 city = 'Maputo'
 print(len(city))
 print(city[3])
+
+"here" in "Where's Waldo?"
---
+++ 01-programming/composing-programs/02-building-abstractions-with-data/05-mutable-data.py
@@ -0,0 +1,5 @@
+from datetime import date
+
+tues = date(2014, 4, 13)
+print(date(2014, 5, 19) - tues)
+print(tues.year)
