a8591549464610a786fd395b80559034f0b60d57
Tomas <tomas@localhost.localdomain>
2023-01-24 09:22:38 +0200

    bubble sort added

---
+++ 02-data-structures/javascript/sort/01-bubble-sort.js
@@ -0,0 +1,49 @@
+/**
+ * 
+ * @param {Array} arr 
+ */
+function bubbleSort(arr) {
+    //loop over first element
+    for (let i = 0; i < arr.length; i++) {
+        //loop over second element
+        for (let j = 0; j < arr.length; j++) {
+            console.log({ elem1: arr[j], elem2: arr[j + 1] })
+            //store first element value
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                // swap
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+            console.log({ arr })
+        }
+    }
+    return arr
+}
+
+/**
+ * 
+ * @param {Array} arr 
+ */
+function optimizedBubbleSort(arr) {
+    //loop over first element
+    for (let i = arr.length; i > 0; i--) {
+        //loop over second element
+        for (let j = 0; j < i - 1; j++) {
+            console.log({ elem1: arr[j], elem2: arr[j + 1] })
+            //store first element value
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                // swap
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+            console.log({ arr })
+        }
+    }
+    return arr
+}
+
+// console.log(bubbleSort([5, 24, 23, 8, 92, 65, 1, 34, 85, 6]))
+// console.log(optimizedBubbleSort([5, 24, 23, 8, 92, 65, 1, 34, 85, 6]))
+console.log(optimizedBubbleSort([5, 24, 23, 8, 1,6]))
