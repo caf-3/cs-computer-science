2ee9c20bd49796f4406882a98e72edfd813a85e6
Tomas <tomas@localhost.localdomain>
2023-01-25 21:51:41 +0200

    selection sort improved

--- 02-data-structures/javascript/sort/02-selection.js
+++ 02-data-structures/javascript/sort/02-selection.js
@@ -6,25 +6,24 @@ function naiveSelectionSort(arr) {
     // loop over first element
     let swaped
     for (let i = 0; i < arr.length; i++) {
-        let minimum = arr[i]
-        let minimumIndex
+        let minimum = i
         swaped = false
-        for (let j = i+1; j < arr.length; j++) {
+        for (let j = i + 1; j < arr.length; j++) {
             // compare the actual minimun with the the current vallue
-            if (minimum > arr[j]) {
-                minimum = arr[j]
-                minimumIndex = j
+            if (arr[minimum] > arr[j]) {
+                minimum = j
                 swaped = true
             }
         }
         if (swaped) {
             let temp = arr[i]
-            arr[i] = arr[minimumIndex]
-            arr[minimumIndex] = temp
+            arr[i] = arr[minimum]
+            arr[minimum] = temp
         }
 
     }
     return arr
 }
 
-console.log(naiveSelectionSort([5, 2, 3, 6, 1, 7, 29, 24, 16]))
+// console.log(naiveSelectionSort([5, 2, 3, 6, 1, 7, 29, 24, 16]))
+console.log(naiveSelectionSort([5, 2, 0, 3, 6, 1]))
