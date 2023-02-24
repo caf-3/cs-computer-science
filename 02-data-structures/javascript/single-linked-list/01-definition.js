6a744b2a65675ca2bb974ea968db051d04abdd43
Tomas <tomas@localhost.localdomain>
2023-02-24 04:51:03 +0200

    shift added

--- 02-data-structures/javascript/single-linked-list/01-definition.js
+++ 02-data-structures/javascript/single-linked-list/01-definition.js
@@ -15,25 +15,27 @@
 /**
  * @type {NodeClass}
  */
-class ListNode{
+class ListNode {
     /**
      * 
      * @param {any} val - Node value
      * @example
      * const node = new Node("A value")
      */
-    constructor(val){
+    constructor(val) {
         this.val = val
-        /** @type any */
+
+        /** @type {NodeClass | null} */
         this.next = null
     }
 }
 
-class SinglyLinkedList{
-    constructor(){
+class SinglyLinkedList {
+    constructor() {
         /** @type {NodeClass | null} */
         this.head = null
-        
+
+        /** @type {NodeClass | null} */
         this.tail = null
         this.length = 0
     }
@@ -42,40 +44,56 @@ class SinglyLinkedList{
      * 
      * @param {any} value - A value to be inserted in the end
      */
-    push(value){
+    push(value) {
         const newNode = new ListNode(value)
-        if(!this.head) {
+        if (!this.head) {
             this.head = newNode
             this.tail = newNode
         }
         // because of intelliSense (on @vsCode) we added the bellow if statement instead of else on the above if 
-        if(this.tail) {
+        if (this.tail) {
             this.tail.next = newNode
             this.tail = newNode
-            
+
         }
         this.length += 1
         return this
     }
 
-    pop(){
+    pop() {
         // base case
-        if(!this.length) return
+        if (!this.length) return
         //  current list value (starting from head)
         let current = this.head
         /** @type any - a variable to store the previous value before the tail */
         let previous
         /** @type {NodeClass | null} - previous node before tail */
         let previousNode = current
-        while(current){
-            if(current.next) previousNode = current
+        while (current) {
+            if (current.next) previousNode = current
             previous = current.val
             current = current.next
         }
-        if(previousNode) previousNode.next = null
+        if(this.length == 0){
+            this.tail = null
+            this.head = null
+        }
+        if (previousNode) previousNode.next = null
         this.tail = previousNode
         this.length--
         return previous
+    }
+
+    shift() {
+        if (!this.length) return
+        const removedHead = this.head
+        this.head = removedHead?.next || null
+        this.length--
+        if(this.length == 0){
+            this.tail = null
+        }
+        return removedHead
+
     }
 }
 
@@ -86,5 +104,12 @@ list.push(3)
 list.push(13)
 list.push(8)
 console.log(list)
-console.log(list.pop())
+console.log("POP", list.pop())
+console.log(list)
+console.log("SHIFT", list.shift())
+console.log("SHIFT", list.shift())
+console.log("SHIFT", list.shift())
+console.log("SHIFT", list.shift())
+console.log("SHIFT", list.shift())
+console.log("SHIFT", list.shift())
 console.log(list)
