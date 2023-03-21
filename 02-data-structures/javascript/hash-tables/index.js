8fb690fe4d67d956a6d9ff28e4fb9e8ee4b7ebac
Tomas <tomas@localhost.localdomain>
2023-03-21 04:11:02 +0200

    get added

--- 02-data-structures/javascript/hash-tables/index.js
+++ 02-data-structures/javascript/hash-tables/index.js
@@ -38,7 +38,6 @@ class HashTable {
     set(key, value) {
         // get key index
         const index = this.#hash(key)
-        console.log({ index })
 
         // if there is no data at this index we create a empty array
         if (!this.keyMap[index]) this.keyMap[index] = []
@@ -46,12 +45,32 @@ class HashTable {
         this.keyMap[index].push([key, value])
     }
 
+    /**
+     * Get value by key
+     * @param {any} key 
+     */
+    get(key) {
+        const index = this.#hash(key)
+
+        if (this.keyMap[index]) {
+
+            for(let i = 0; i < this.keyMap[index].length; i++ ){
+                const hashTable = this.keyMap[index][i]
+                if(hashTable[0] == key) return hashTable[1]
+            }
+
+        }
+        return
+
+    }
+
 }
 
-let ht = new HashTable()
+let ht = new HashTable(4)
 ht.set("i love", "Mingas")
 ht.set("i was eating", "Beans")
 ht.set("Listening to", "J Cole")
 ht.set("song", "ATM")
 ht.set("verse", "uno dois tres")
 console.log(ht.keyMap)
+console.log(ht.get("Listening to"))
