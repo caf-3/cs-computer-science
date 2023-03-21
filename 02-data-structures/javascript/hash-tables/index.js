94fc79c0a4c41d1bc3a385f3e87b307c970757da
Tomas <tomas@localhost.localdomain>
2023-03-21 03:30:50 +0200

    hash table class added

--- 02-data-structures/javascript/hash-tables/index.js
+++ 02-data-structures/javascript/hash-tables/index.js
@@ -1,19 +1,31 @@
 // @ts-check
 
-/**
- * 
- * @param {String} key 
- * @param {Number} arrayLen 
- */
-function hash(key, arrayLen){
-    let total = 0
-    let WEIRD_PRIME = 31
-
-    for(let i = 0; i < Math.min(key.length, 100); i++){
-        let char = key[i]
-        let value = char.charCodeAt(0) - 96
-        total = (total * WEIRD_PRIME + value) % arrayLen
+class HashTable {
+    /**
+     * 
+     * @param {Number} size 
+     */
+    constructor(size = 53) {
+        this.keyMap = new Array(size)
     }
 
-    return total
+    /**
+     * 
+     * @param {String} key 
+     * @param {Number} arrayLen 
+     */
+    #hash(key, arrayLen) {
+        let total = 0
+        let WEIRD_PRIME = 31
+
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96
+            total = (total * WEIRD_PRIME + value) % arrayLen
+        }
+
+        return total
+    }
+
 }
+
